b97162f1e0b097575c4dd6327688c9f1d2ac9734
queueji-hub <queueji-hub@users.noreply.github.com>
2026-05-24 12:24:57 +0700

    fix

--- database/connection.py
+++ database/connection.py
@@ -1,5 +1,6 @@
-import psycopg2
 import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
@@ -10,8 +11,14 @@ DB_CONFIG = {
 }
 
 def get_connection():
-    return psycopg2.connect(**DB_CONFIG)
-
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT", 5432),
+        cursor_factory=RealDictCursor
+    )
 
 # =========================================================
 # SAFE EXECUTOR (ใช้ทั้งระบบ)
