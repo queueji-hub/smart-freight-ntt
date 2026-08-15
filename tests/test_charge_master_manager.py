41700bdeb864a53c0a9a68ac3130bd76f3e92209
queueji-hub <queueji@gmail.com>
2026-08-15 12:28:29 +0700

    test(master): add charge master contract tests

---
+++ tests/test_charge_master_manager.py
@@ -0,0 +1,43 @@
+import managers.charge_master_manager as charges
+
+
+def test_get_charge_normalizes_code(monkeypatch):
+    captured = {}
+
+    class FakeCursor:
+        def execute(self, query, params=None):
+            captured["query"] = query
+            captured["params"] = params
+        def fetchone(self):
+            return None
+    class FakeConn:
+        def cursor(self):
+            return FakeCursor()
+    monkeypatch.setattr(charges, "get_connection", lambda: _Ctx(FakeConn()))
+    monkeypatch.setattr(charges, "get_current_tenant_id", lambda: "t1")
+
+    assert charges.get_charge(" thc ") is None
+    assert captured["params"] == ("t1", "THC")
+
+
+def test_list_charges_returns_empty_when_migration_is_not_applied(monkeypatch):
+    class FakeCursor:
+        def execute(self, query, params=None):
+            raise RuntimeError("relation charge_master does not exist")
+        def fetchall(self):
+            return []
+    class FakeConn:
+        def cursor(self):
+            return FakeCursor()
+    monkeypatch.setattr(charges, "get_connection", lambda: _Ctx(FakeConn()))
+    monkeypatch.setattr(charges, "get_current_tenant_id", lambda: "t1")
+    assert charges.list_charges() == []
+
+
+class _Ctx:
+    def __init__(self, value):
+        self.value = value
+    def __enter__(self):
+        return self.value
+    def __exit__(self, *args):
+        return False
